388589526f82884f25a416bb56fee0317cfcd38c
kavya9 <vkavya.13.cse@anits.edu.in>
2015-11-15 07:51:18 +0530

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -19,7 +19,55 @@ NOTES: Don't create new string.
 
 #include <stdio.h>
 
-
 void number_to_str(float number, char *str,int afterdecimal){
-	
+	int intPart, fracToInt,p=0,i,flag=0;
+	float fracPart;
+	char temp;
+	temp = afterdecimal;
+	if (number<0){ number *= -1; flag = 1; }
+	intPart = number;
+	fracPart = number - intPart;
+	//converting fraction part into integer with "afterdecimal" no of digits
+	while (afterdecimal > 0)
+	{
+		fracPart = 10 * fracPart;
+		afterdecimal--;
+	}
+	fracToInt = fracPart;
+	//converting fraction part to string
+	while (fracToInt>0)
+	{
+		str[p] = (fracToInt % 10) + 48;
+		fracToInt /= 10;
+		p++;
+	}
+	if (temp != 0)
+	{
+		str[p] = '.';
+		p++;
+	}
+	//converting integer part to string
+	while (intPart>0)
+	{
+		str[p] = (intPart % 10) + 48;
+		intPart /= 10;
+		p++;
+	}
+	if (flag)
+	{
+		str[p] = '-';
+		p++;
+	}
+	str[p] = '\0';
+	p = --p;
+	i = 0;
+	//reversing the string
+	while (i<p)
+	{
+		temp = str[i];
+		str[i] = str[p];
+		str[p] = temp;
+		i++;
+		p--;
+	}
 }
